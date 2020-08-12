ef54d94e8bd363bbd8b7fac5b2f17eadea9e2101
Jack Storrs <jackstorrs8@gmail.com>
2020-08-11 22:38:00 -0400

    Push 12-Stu_Structured-Tests w/ comments for positive,negative,exception tests + arrange/act/assert comments

--- 10-OOP/01-Activities/12-Stu_Structured-Tests/Solved/test/child.test.js
+++ 10-OOP/01-Activities/12-Stu_Structured-Tests/Solved/test/child.test.js
@@ -2,44 +2,62 @@ const Child = require("../child");
 
 describe("Child", () => {
   describe("Initialization", () => {
+    //Positive test
     it("should create an object with a name and age if provided valid arguments", () => {
+      //Arrange
       const child = new Child("Sarah", 3);
 
+      //Assert
       expect(child.name).toEqual("Sarah");
       expect(child.age).toEqual(3);
     });
 
+    //Exception test
     it("should throw an error if provided no arguments", () => {
+      //Arrange
       const cb = () => new Child();
 
+      //Assert
       expect(cb).toThrow();
     });
 
+    //Exception test
     it("should throw an error if not provided an age", () => {
+      //Arrange
       const cb = () => new Child("Sarah");
       const err = new Error("Expected parameter 'age' to be a non-negative number");
 
+      //Assert
       expect(cb).toThrowError(err);
     });
 
+    //Exception test
     it("should throw an error if 'name' is not a string", () => {
+      //Arrange
       const cb = () => new Child(3, 2);
       const err = new Error("Expected parameter 'name' to be a non-empty string");
 
+      //Assert
       expect(cb).toThrowError(err);
     });
 
+    //Exception test
     it("should throw an error if 'age' is not a number", () => {
+      //Arrange
       const cb = () => new Child("Sarah", "2");
       const err = new Error("Expected parameter 'age' to be a non-negative number");
 
+      //Assert
       expect(cb).toThrowError(err);
     });
 
+    //Exception test
     it("should throw an error if 'age' is less than 0", () => {
+      //Arrange
       const cb = () => new Child("Sarah", -1);
       const err = new Error("Expected parameter 'age' to be a non-negative number");
 
+      //Assert
       expect(cb).toThrowError(err);
     });
   });
--- 10-OOP/01-Activities/12-Stu_Structured-Tests/Solved/test/dayCare.test.js
+++ 10-OOP/01-Activities/12-Stu_Structured-Tests/Solved/test/dayCare.test.js
@@ -3,34 +3,47 @@ const Child = require("../child");
 
 describe("DayCare", () => {
   describe("Initialization", () => {
+    //Positive test
     it("should create an object with 'children' array, 'capacity' number, and 'ageLimit' number", () => {
+      //Arrange
       const dayCare = new DayCare();
 
+      //Assert
       expect(dayCare).toEqual({ children: [], capacity: 3, ageLimit: 6 });
     });
   });
 
   describe("addChild", () => {
+    //Positive test
     it("should add a child to the 'children' array", () => {
+      //Arrange
       const child = new Child("Tammy", 1);
       const dayCare = new DayCare();
 
+      //Act
       dayCare.addChild(child);
 
+      //Assert
       expect(dayCare.children.length).toEqual(1);
       expect(dayCare.children[0]).toBe(child);
     });
 
+    //Negative test
     it("should not add a child over the 'ageLimit'", () => {
+      //Arrange
       const child = new Child("Tammy", 8);
       const dayCare = new DayCare();
 
+      //Act
       dayCare.addChild(child);
 
+      //Assert
       expect(dayCare.children.length).toEqual(0);
     });
 
+    //Negative test
     it("should not add a child if already at capacity", () => {
+      //Arrange
       const dayCare = new DayCare();
       const child = new Child("Alice", 4);
       dayCare.children = [
@@ -39,12 +52,16 @@ describe("DayCare", () => {
         new Child("Alvin", 1)
       ];
 
+      //Act
       dayCare.addChild(child);
 
+      //Assert
       expect(dayCare.children.length).toEqual(3);
     });
 
+    //Exception test
     it("should throw an error if not provided a Child object as an argument", () => {
+      //Arrange
       const err = new Error(
         "Expected parameter 'child' to be an instance of Child"
       );
@@ -53,20 +70,25 @@ describe("DayCare", () => {
         dayCare.addChild();
       };
 
+      //Assert
       expect(cb).toThrowError(err);
     });
   });
 
   describe("pickupChild", () => {
+    //Positive test
     it("should remove the first child found with a given name from 'children' and return it", () => {
+      //Arrange
       const dayCare = new DayCare();
       const child1 = new Child("Tammy", 1);
       const child2 = new Child("Mark", 2);
       const child3 = new Child("Alvin", 1);
       dayCare.children = [child1, child2, child3];
 
+      //Act
       const removed = dayCare.pickupChild(child2.name);
 
+      //Assert lots of things
       expect(removed).toBe(child2);
       expect(dayCare.children.length).toEqual(2);
       expect(
@@ -74,15 +96,19 @@ describe("DayCare", () => {
       ).toEqual(false);
     });
 
+    //Negative test
     it("should return undefined and remove no children if child is not in 'children'", () => {
+      //Arrange
       const dayCare = new DayCare();
       const child1 = new Child("Tammy", 1);
       const child2 = new Child("Mark", 2);
       const child3 = new Child("Alvin", 1);
       dayCare.children = [child1, child2, child3];
-
+      
+      //Act
       const removed = dayCare.pickupChild("Fred");
 
+      //Assert
       expect(typeof removed).toEqual("undefined");
       expect(dayCare.children).toEqual([child1, child2, child3]);
     });
